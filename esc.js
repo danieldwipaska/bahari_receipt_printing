789863bf5673550e4104924e49135244dfe362ae
Daniel <danielkamasi@outlook.com>
2025-08-06 23:26:03 +0700

    change again

--- esc.js
+++ esc.js
@@ -60,16 +60,7 @@ function formatCurrency(amount) {
 }
 
 function formatDate(date) {
-    const options = { 
-        day: '2-digit', 
-        month: '2-digit', 
-        year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: true
-    };
-    return date.toLocaleString('id-ID', options);
+    return date.toLocaleString();
 }
 
 function generateReceipt(data, isChecker) {
