7a39b81d6a46ec2e0671ff95a9e44462ca5e45e0
Daniel <danielkamasi@outlook.com>
2025-08-19 20:42:39 +0700

    add final balance and remove esc

--- index.js
+++ index.js
@@ -2,39 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 const { promisify } = require('util');
-const { generateReceipt } = require('./esc');
 
 const app = express();
 app.use(express.json());
 
-/**
- * Print with IWARE device
- * @param {Object} receiptData - Receipt data
- * @param {string} receiptData.storeName - Store name
- * @param {string} receiptData.address - Store Address
- * @param {Date} receiptData.date - Receipt date
- * @param {string} receiptData.receiptNumber - Receipt number
- * @param {string} receiptData.servedBy - Crew name who served
- * @param {string} receiptData.customerName - Customer name
- * @param {Object} receiptData.items - Order items
- * @param {string} receiptData.items.name - Item name
- * @param {number} receiptData.items.quantity - Item quantity or amount
- * @param {number} receiptData.items.price - Item price
- * @param {number} receiptData.items.discountPercent - Discount percentage
- * @param {number} receiptData.items.discountedPrice - Price after discount
- * @param {boolean} receiptData.includedTaxService - Whether or not the order includes tax and service 
- * @param {number} receiptData.taxPercent - Tax percentage
- * @param {number} receiptData.servicePercent - Service percentage
- * @param {number} receiptData.subtotal - Receipt subtotal value
- * @param {number} receiptData.total - Receipt total value
- * @param {string} receiptData.note - Receipt total value
- * @returns {Object} { success: true/false }
- * @throws {Error} If printing error
- */
-async function printToIware(receiptData, devicePath = process.env.PRINTER_DEVICE_PATH, isChecker = false) {
+async function printToIware(data, devicePath = process.env.PRINTER_DEVICE_PATH) {
   try {
     // Write ke device menggunakan fs.writeFile
-    await promisify(fs.writeFile)(devicePath, Buffer.from(generateReceipt(receiptData, isChecker), 'binary'));
+    await promisify(fs.writeFile)(devicePath, Buffer.from(data, 'binary'));
 
     return { success: true };
   } catch (error) {
@@ -42,9 +17,15 @@ async function printToIware(receiptData, devicePath = process.env.PRINTER_DEVICE
   }
 }
 
+// data should be in the esc/pos-formatted string
 app.post('/print', async (req, res) => {
   try {
-    const { data, isChecker, printer } = req.body;
+    const { data, printer } = req.body;
+    if (!data)
+      return res.status(400).json({
+        success: false,
+        error: 'Data is required',
+      });
 
     const printerOptions = {
       first_printer: process.env.FIRST_PRINTER || '',
@@ -54,23 +35,12 @@ app.post('/print', async (req, res) => {
 
     const devicePath = printerOptions[printer];
 
-    data.date = new Date(data.date);
-
-    if (!data)
-      return res.status(400).json({
-        success: false,
-        error: 'Data is required',
-      });
-
     let printed = false;
     let lastError = '';
 
     try {
-      if (isChecker) {
-        await printToIware(data, devicePath, isChecker);
-      } else {
-        await printToIware(data, devicePath);
-      }
+      await printToIware(data, devicePath);
+
       console.log(`✓ Printed successfully to ${devicePath ?? process.env.DEFAULT_DEVICE_PATH}`);
       printed = true;
     } catch (error) {
